cdfd0422b2ee1d5569e9c2dc631c753a76117c19
Seb Jones <jonesseb100@gmail.com>
2017-11-15 12:59:34 +0000

    Added lines to read in lines of .txt file

--- dataWaveAna.py
+++ dataWaveAna.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 from ROOT import TGraph
 
+filename  = 'dataWave.txt'
+file = open(filename, "r")
+firstline = file.readline()
+nWords = 0
+
+for word in firstline.split():
+    print(word)
+    nWords += 1
