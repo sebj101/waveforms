758d8a624bc92e4c02075c046dcba23d74715a91
Seb Jones <jonesseb100@gmail.com>
2017-11-28 13:51:37 +0000

    Appends data poinst to a list

--- dataWaveAna.py
+++ dataWaveAna.py
@@ -1,11 +1,21 @@
 #!/usr/bin/env python
+# Reads in a line from a data file and plots the graph of it
+
 from ROOT import TGraph
 
-filename  = 'dataWave.txt'
+filename  = 'data.txt'
 file = open(filename, "r")
 firstline = file.readline()
 nWords = 0
 
+dataPoints =[] # list to store data points in 
+
+# Loops over all the words in the first line
 for word in firstline.split():
+    # Appends data points to list
+    dataPoints.append(word)
     print(word)
     nWords += 1
+
+sum = dataPoints[3] + dataPoints[4]
+print("Sum of two points = " + str(sum))
